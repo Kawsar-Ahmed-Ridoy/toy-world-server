f599a2c0e6891b46a4ea69a0936d454f08825044
Kawsar-Ahmed-Ridoy <kawsarahmedridoy1@gmail.com>
2023-06-09 16:46:25 +0600

    express server connect mongobd database

--- index.js
+++ index.js
@@ -1,10 +1,54 @@
 const express = require('express');
 const cors = require('cors')
 const app = express()
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 app.use(cors())
 app.use(express.json())
+
+// dbuser= toyWorld
+// bdpass=CeDr8OKos5x0igtO
+
+
+
+const uri = `mongodb+srv://toyWorld:CeDr8OKos5x0igtO@cluster0.dgcetkk.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const categoryItem = client.db('toyWorld').collection('category')
+
+    app.get('/categorys', async (req, res)=>{
+      const category = await categoryItem.find({}).toArray();
+      res.send(category);
+    })
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("toyWorld").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+
+
+  } finally {
+    // Ensures that the client will close when you finish/error
+  }
+}
+run().catch(console.dir);
+
+
 
 app.get('/', (req, res) => {
     res.send('Toy World server is running')
