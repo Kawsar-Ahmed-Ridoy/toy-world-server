d4ce075b9a165b3dda83d2eb2b51afbac968cece
Kawsar-Ahmed-Ridoy <kawsarahmedridoy1@gmail.com>
2023-06-10 14:53:15 +0600

    toy delete functionality added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors')
 const app = express()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 app.use(cors())
@@ -58,6 +58,14 @@ async function run() {
     app.get('/categoryDetails', async (req, res)=>{
       const category = await categoryDetails.find({}).toArray();
       res.send(category);
+    })
+
+    app.delete('/myToys/:id', async (req, res) =>{
+      const id = req.params.id;
+      console.log('delete ', id);
+      const query = {_id: new ObjectId(id)}
+      const result = await addAToy.deleteOne(query)
+      res.send(result)
     })
 
 
